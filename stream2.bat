7482f5a12403749712c80e928e21c56b5b0b745b
n00shE <ryanblanchard00@gmail.com>
2019-02-19 12:00:41 -0800

    Added README and client files

---
+++ stream2.bat
@@ -0,0 +1,1 @@
+C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0 --gst-debug-level=2 udpsrc port=5804 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96, a-framerate=(string)20" ! rtpjitterbuffer ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink sync=false
